f614f14202253ad8329d32327ce1b221f24bec1e
wangyong <yongwc@yeah.net>
2013-11-04 23:36:30 +0800

    just make a compile success server and client, but when they communicated with each other, the server crash ohh

--- client/client.c
+++ client/client.c
@@ -1,5 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <unistd.h>
 #include <arpa/inet.h>
-#include "include/message_def.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define SMOOTH_MESSAGE_HEAD_LEN 12 
+
+#define SMOOTH_LOGIN 0
+#define SMOOTH_MESSAGE_CHART_TEXT 1
+
+#define SMOOTH_CHART_MESSAGE_BUFFER_MAX_LEN 1024
+
+#define SMOOTH_MSG_DEFAULT_HOST_IP "127.0.0.1"
+#define SMOOTH_MSG_DEFAULT_PORT 45000
+
+#define SMOOTH_CLIENT_PROMOT "smooth>> "
+
+struct msg_head
+{
+	int version;
+	int type;
+	int length;
+};
+
+typedef struct msg_string
+{
+	char * data;
+	int length;
+} msg_string_t;
+
+struct msg_chart_text
+{
+	struct msg_chart_text * next;
+
+	msg_string_t from;
+	msg_string_t to;
+	msg_string_t msg;
+
+	char buffer[0];
+};
+
+void smooth_msg_manager_push_msg(struct msg_chart_text * chart_text)
+{
+	char * p;
+	char back;
+
+	p = chart_text->from.data + chart_text->from.length;
+
+	back = *p;
+	*p = '\0';
+	printf("Message\nFrom: %s\n", chart_text->from.data);
+	*p = back;
+
+	p = chart_text->msg.data + chart_text->msg.length;
+	back = *p;
+	*p = '\0';
+	printf("Content: %s\n", chart_text->msg.data);
+	*p = back;
+}
+
 
 int smooth_msg_pack_head(char * buffer, int version, int type, int length)
 {
@@ -43,18 +107,25 @@ int smooth_msg_pack_login(char * buffer, char * account, char * passwd)
 	return p - buffer;
 }
 
-int smooth_msg_pack_chart_text(char * buffer, char * who, char * msg)
+int smooth_msg_pack_chart_text(char * buffer, char * from, char * to, char * msg)
 {
 	char * p;
 	int len;
 
 	p = buffer;
 
-	len = strlen(who);
+	len = strlen(from);
 	*(int *)p = htonl(len);
 	p += 4;
 
-	memcpy(p, who, len);
+	memcpy(p, from, len);
+	p += len;
+
+	len = strlen(to);
+	*(int *)p = htonl(len);
+	p += 4;
+
+	memcpy(p, to, len);
 	p += len;
 
 	len = strlen(msg);
@@ -74,7 +145,7 @@ int smooth_msg_pack_chart_text(char * buffer, char * who, char * msg)
 void smooth_msg_read_msg_head(int sockfd, char * buffer, struct msg_head * head)
 {
 	char * p;
-	int left = MESSAGE_HEAD_LEN;
+	int left = SMOOTH_MESSAGE_HEAD_LEN;
 	int rbyte;
 
 	p = buffer;
@@ -96,7 +167,6 @@ void smooth_msg_read_msg_head(int sockfd, char * buffer, struct msg_head * head)
 
 	head->length = ntohl(*(int *)p);
 	p += 4;
-
 }
 
 /*
@@ -119,23 +189,23 @@ void smooth_msg_read_msg_content(int sockfd, char * buffer, int length)
 	}
 }
 
-struct msg_str
-{
-	char * data;
-	int length;
-};
-
-struct msg_chart_text
-{
-	struct msg_chart_text * next;
-
-	struct msg_str who;
-	struct msg_str msg;
-	char buffer[0];
-};
-
 struct msg_chart_text * gpstMsgChartTextPool;
 
+int smooth_msg_initailize_chart_text_pool(int size)
+{
+	int i;
+	struct msg_chart_text * chart_text;
+
+	for (i = 0; i < size; ++i)
+	{
+		chart_text = malloc(sizeof(struct msg_chart_text) + SMOOTH_MESSAGE_HEAD_LEN + SMOOTH_CHART_MESSAGE_BUFFER_MAX_LEN);
+
+		assert(chart_text && "chart text struct alloc failed");
+
+		chart_text->next = gpstMsgChartTextPool;
+		gpstMsgChartTextPool = chart_text;
+	}
+}
 
 struct msg_chart_text * smooth_msg_get_chart_text_node()
 {
@@ -143,11 +213,7 @@ struct msg_chart_text * smooth_msg_get_chart_text_node()
 
 	if (!gpstMsgChartTextPool)
 	{
-		gpstMsgChartTextPool = malloc(sizeof(struct msg_chart_text) + MESSAGE_CHART_TEXT_CONTENT_MAX_LENGTH);
-		if (!gpstMsgChartTextPool)
-			return NULL;
-
-		gpstMsgChartTextPool->next = NULL;
+		smooth_msg_initailize_chart_text_pool(20);
 	}
 
 	//TODO here need a lock
@@ -167,12 +233,11 @@ void smooth_msg_free_chart_msg_buffer(struct msg_chart_text * chart_text)
 void smooth_msg_read_msg(int sockfd)
 {
 	char * p;
-	char * buffer;
 	int left;
 	int rbyte;
 	struct msg_head head;
 	struct msg_chart_text * chart_text;
-	char msg_head_buf[MESSAGE_HEAD_LEN];
+	char msg_head_buf[SMOOTH_MESSAGE_HEAD_LEN];
 
 	smooth_msg_read_msg_head(sockfd, msg_head_buf, &head);
 
@@ -182,51 +247,64 @@ void smooth_msg_read_msg(int sockfd)
 			chart_text = smooth_msg_get_chart_text_node();
 
 			p = chart_text->buffer;
-			memcpy(p, msg_head_buf, MESSAGE_HEAD_LEN);
-			p += MESSAGE_HEAD_LEN;
+			memcpy(p, msg_head_buf, SMOOTH_MESSAGE_HEAD_LEN);
+			p += SMOOTH_MESSAGE_HEAD_LEN;
 
 			smooth_msg_read_msg_content(sockfd, p, head.length);
 
-			chart_text->who.length = ntohl(*(int *)p);
+			chart_text->from.length = ntohl(*(int *)p);
 			p += 4;
 
-			chart_text->who.data = p;
-			p += chart_text->who.length;
+			chart_text->from.data = p;
+			p += chart_text->from.length;
+
+			chart_text->to.length = ntohl(*(int *)p);
+			p += 4;
+
+			chart_text->to.data = p;
+			p += chart_text->to.length;
 
 			chart_text->msg.length = ntohl(*(int *)p);
 			p += 4;
-			chart_text->who.data = p;
+
+			chart_text->msg.data = p;
+			p += chart_text->msg.length;
 
 			smooth_msg_manager_push_msg(chart_text);
 			break;
 		default:
-			smooth_msg_free_chart_msg_buffer(buffer);
 			assert(0 && "message head with an invalid type");
 			return;
 	}
 }
 
-int smooth_msg_initailize_connection(char * host_ip, int host_port)
+int smooth_msg_initailize_connection(char * host_ip, short host_port)
 {
 	int sockfd;
-	struct sockaddr addr;
+	int ret;
+	struct sockaddr_in addr;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd != 0)
-		return -1;
-
-	addr.sa_family = AF_INET;
-	addr.sa_port = htons(host_port);
-	ret = inet_pton(AF_INET, host_ip, &addr.sa_addr);
-	if (ret != 0)
+	if (sockfd == -1)
 	{
+		fprintf(stderr, "create socket failed\n");
+		return -1;
+	}
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(host_port);
+	ret = inet_pton(AF_INET, host_ip, &addr.sin_addr);
+	if (ret != 1)
+	{
+		fprintf(stderr, "trans ip failed\n");
 		close(sockfd);
 		return -1;
 	}
 
-	ret = connect(sockfd, &addr, sizeof(struct sockaddr));
+	ret = connect(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr));
 	if (ret != 0)
 	{
+		fprintf(stderr, "connect failed\n");
 		close(sockfd);
 		return -1;
 	}
@@ -234,7 +312,43 @@ int smooth_msg_initailize_connection(char * host_ip, int host_port)
 	return sockfd;
 }
 
-struct msg_manager
+int sockfd_conn;
+
+int main()
 {
-	int sockfd;
-};
+	char * line = NULL;
+	size_t line_len;
+	int input_count;
+	int len;
+
+	char * msg_from = "Bob";
+	char * msg_to = "Alice";
+
+	char msg_buffer[SMOOTH_CHART_MESSAGE_BUFFER_MAX_LEN];
+	char * p;
+
+	smooth_msg_initailize_chart_text_pool(20);
+
+	sockfd_conn = smooth_msg_initailize_connection(SMOOTH_MSG_DEFAULT_HOST_IP, SMOOTH_MSG_DEFAULT_PORT);
+	if (sockfd_conn == -1)
+	{
+		fprintf(stderr, "client connect to %s:%d failed\n", SMOOTH_MSG_DEFAULT_HOST_IP, SMOOTH_MSG_DEFAULT_PORT);
+		return 0;
+	}
+
+	while (1)
+	{
+		printf(SMOOTH_CLIENT_PROMOT);
+		input_count = getline(&line, &line_len, stdin);
+		if (input_count == -1)
+			return -1;
+
+		line[input_count - 1] = '\0';
+
+		len = smooth_msg_pack_chart_text(msg_buffer + SMOOTH_MESSAGE_HEAD_LEN, msg_from, msg_to, line);
+
+		smooth_msg_pack_head(msg_buffer, 1, SMOOTH_MESSAGE_CHART_TEXT, len);
+
+		write(sockfd_conn, msg_buffer, len + SMOOTH_MESSAGE_HEAD_LEN);
+	}
+}
--- server/server.c
+++ server/server.c
@@ -1,13 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
 
 #define MSG_SUCCESS 			0
 #define MSG_ERR_FAILED			1
 #define MSG_ERR_AGAIN			2
 
+#define SMOOTH_MSG_LOGIN 0
+#define SMOOTH_MSG_CHART_TEXT 1
+
+#define SMOOTH_SET_FD_NONBLOCK(fd) fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK)
+
+#define SMOOTH_MSG_HEAD_LEN 12
+#define SMOOTH_MSG_CHART_TEXT_CONTENT_MAX_LEN 1024
+
+#define SMOOTH_HOST_DEFAULT "127.0.0.1"
+#define SMOOTH_PORT_DEFAULT 45000
+
 typedef struct head_buffer 
 {
 	char * p;
 	int left;
-	char buffer[SMOOTH_MSG_HEAD_MAX_LEN];
+	char buffer[SMOOTH_MSG_HEAD_LEN];
 } head_buffer_t;
 
 struct connection 
@@ -31,8 +54,6 @@ typedef struct msg_string
 	int length;
 	char * data;
 } msg_string_t;
-
-#define SMOOTH_SET_FD_NONBLOCK(fd) fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK)
 
 struct msg_login
 {
@@ -60,6 +81,129 @@ typedef struct msg_chart_text
 	int left;
 	char msg_buffer[0];
 } msg_chart_text_t;
+
+extern int smooth_msg_process_head(struct connection * conn);
+extern void smooth_msg_parse_head(head_buffer_t * head_buf, msg_head_t * head);
+extern void smooth_msg_init_head_buffer(head_buffer_t * head_buffer);
+
+msg_chart_text_t * gp_chart_text_pool;
+
+int smooth_msg_initailize_chart_text_pool(int size)
+{
+	int i;
+	msg_chart_text_t * chart_text;
+
+	for (i = 0; i < size; ++i)
+	{
+		chart_text = malloc(sizeof(msg_chart_text_t) + SMOOTH_MSG_HEAD_LEN + SMOOTH_MSG_CHART_TEXT_CONTENT_MAX_LEN);
+
+		chart_text->next = gp_chart_text_pool;
+
+		gp_chart_text_pool = chart_text;
+	}
+}
+
+msg_chart_text_t * smooth_msg_get_msg_chart_text()
+{
+	msg_chart_text_t * chart_text;
+
+	if (!gp_chart_text_pool)
+	{
+		smooth_msg_initailize_chart_text_pool(20);
+	}
+
+	chart_text = gp_chart_text_pool;
+
+	gp_chart_text_pool = chart_text->next;
+
+	return chart_text;
+}
+
+void smooth_msg_free_chart_text(msg_chart_text_t * chart_text)
+{
+	chart_text->next = gp_chart_text_pool;
+
+	gp_chart_text_pool = chart_text;
+}
+
+struct msg_login * smooth_msg_get_login()
+{
+	return malloc(sizeof(struct msg_login));
+}
+
+struct connection * smooth_manager_get_connection()
+{
+	return malloc(sizeof(struct connection));
+}
+
+void smooth_manager_free_connection(struct connection * conn)
+{
+	free(conn);
+}
+
+int smooth_msg_head_handler(struct connection * conn)
+{
+	int left = conn->head.left;
+	char * p = conn->head.p;
+
+	int rbyte;
+
+	while (1)
+	{
+		rbyte = read(conn->sockfd, p, left);
+		if (rbyte == -1)
+		{
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				conn->head.left = left;
+				conn->head.p = p;
+
+				return MSG_ERR_AGAIN;
+			}
+
+			return MSG_ERR_FAILED;
+		}
+
+		p += rbyte;
+		left -= rbyte;
+
+		if (left == 0)
+		{
+			return smooth_msg_process_head(conn);
+		}
+	}
+}
+
+void smooth_msg_manager_transfor_chart_text(msg_chart_text_t * chart_text)
+{
+	char * p;
+	char back;
+
+	p = chart_text->msg_from.data + chart_text->msg_from.length;
+	back = *p;
+	*p = '\0';
+	printf("Message\nFrom:%s\n", chart_text->msg_from.data);
+	*p = back;
+
+	p = chart_text->msg_to.data + chart_text->msg_to.length;
+	back = *p;
+	*p = '\0';
+	printf("To:%s\n", chart_text->msg_to.data);
+	*p = back;
+
+	p = chart_text->msg_content.data + chart_text->msg_content.length;
+	back = *p;
+	*p = '\0';
+	printf("Content:%s\n", chart_text->msg_content.data);
+	*p = back;
+
+	smooth_msg_free_chart_text(chart_text);
+}
+
+void smooth_msg_manager_process_login(struct connection * conn)
+{
+
+}
 
 void smooth_msg_parse_chart_text(msg_chart_text_t * chart_text)
 {
@@ -125,6 +269,7 @@ int smooth_msg_chart_text_handler(struct connection * conn)
 		{
 			smooth_msg_process_chart_text(conn);
 
+			smooth_msg_init_head_buffer(&conn->head);
 			conn->handler = smooth_msg_head_handler;
 			return smooth_msg_head_handler(conn);
 		}
@@ -189,13 +334,14 @@ int smooth_msg_login_handler(struct connection * conn)
 		{
 			smooth_msg_process_login(conn);
 
+			smooth_msg_init_head_buffer(&conn->head);
 			conn->handler = smooth_msg_head_handler;
 			return smooth_msg_head_handler(conn);
 		}
 	}
 }
 
-void smooth_msg_process_head(struct connection * conn)
+int smooth_msg_process_head(struct connection * conn)
 {
 	struct msg_head head;
 	struct msg_chart_text * chart_text;
@@ -203,17 +349,17 @@ void smooth_msg_process_head(struct connection * conn)
 
 	smooth_msg_parse_head(&conn->head, &head);
 
-	switch (head->type)
+	switch (head.type)
 	{
 		case SMOOTH_MSG_CHART_TEXT:
 			chart_text = smooth_msg_get_msg_chart_text();
 
 			conn->content = chart_text;
-			memcpy(chart_text->p, conn->head.buffer, SMOOTH_MSG_HEAD_MAX_LEN);
-			chart_text->p += SMOOTH_MSG_HEAD_MAX_LEN;
+			memcpy(chart_text->p, conn->head.buffer, SMOOTH_MSG_HEAD_LEN);
+			chart_text->p += SMOOTH_MSG_HEAD_LEN;
 
 			chart_text->left = head.length;
-			chart_text->content = p;
+			chart_text->content = chart_text->p;
 
 			conn->handler = smooth_msg_chart_text_handler;
 
@@ -222,11 +368,11 @@ void smooth_msg_process_head(struct connection * conn)
 			login = smooth_msg_get_login();
 
 			conn->content = login;
-			memcpy(chart_text->p, conn->head.buffer, SMOOTH_MSG_HEAD_MAX_LEN);
-			chart_text->p += SMOOTH_MSG_HEAD_MAX_LEN;
+			memcpy(chart_text->p, conn->head.buffer, SMOOTH_MSG_HEAD_LEN);
+			chart_text->p += SMOOTH_MSG_HEAD_LEN;
 
 			chart_text->left = head.length;
-			chart_text->content = p;
+			chart_text->content = chart_text->p;
 
 			conn->handler = smooth_msg_login_handler;
 
@@ -234,7 +380,7 @@ void smooth_msg_process_head(struct connection * conn)
 			break;
 		default:
 			assert(0 && "message type invalid");
-			return;
+			return MSG_ERR_FAILED;
 	}
 }
 
@@ -251,41 +397,10 @@ void smooth_msg_parse_head(head_buffer_t * head_buf, msg_head_t * head)
 	head->length = ntohl(*(int *)p);
 }
 
-int smooth_msg_head_handler(struct connection * conn)
-{
-	int left = conn->head.left;
-	char * p = conn->head.p;
-
-	int rbyte;
-
-	while (1)
-	{
-		rbyte = read(conn->sockfd, p, left);
-		if (rbyte == -1)
-		{
-			if (errno == EAGAIN || errno == EWOULDBLOCK)
-			{
-				conn->head.left = left;
-				conn->head.p = p;
-
-				return MSG_ERR_AGAIN;
-			}
-
-			return MSG_ERR_FAILED;
-		}
-
-		p += rbyte;
-		left -= rbyte;
-
-		if (left == 0)
-		{
-			smooth_msg_process_head(conn);
-			return MSG_SUCCESS;
-		}
-	}
-}
 
 #define SMOOTH_CONNECTION_MAX 1024
+#define SMOOTH_EPOLL_SIZE_DEFAULT SMOOTH_CONNECTION_MAX
+#define SMOOTH_LISTEN_POOL 20
 struct connection * gpstConnectionArray[SMOOTH_CONNECTION_MAX];
 int gConnectionCount;
 
@@ -302,15 +417,26 @@ int smooth_manager_conn_handler(int conn_fd)
 	return conn->handler(conn);
 }
 
-void smooth_manager_listen_handler()
+void smooth_msg_init_head_buffer(head_buffer_t * head_buffer)
 {
+	head_buffer->p = head_buffer->buffer;
+	head_buffer->left = SMOOTH_MSG_HEAD_LEN;
+}
+
+int smooth_manager_listen_handler()
+{
+	char addr_buf[64];
+	struct sockaddr_in addr;
+	int addr_len;
 	struct connection * conn;
 	struct epoll_event event;
 	int conn_fd;
+	int ret;
 
 	while (1)
 	{
-		conn_fd = accept(sockfd, &sockaddr, sockaddr_len);
+		addr_len = sizeof(struct sockaddr);
+		conn_fd = accept(sockfd_listener, (struct sockaddr *)&addr, &addr_len);
 		if (conn_fd == -1)
 		{
 			if (errno == EAGAIN || errno == EWOULDBLOCK)
@@ -318,6 +444,9 @@ void smooth_manager_listen_handler()
 
 			return MSG_ERR_FAILED;
 		}
+
+		inet_ntop(AF_INET, &addr.sin_addr, addr_buf, 64);
+		printf("New connection from %s:%d\n", addr_buf, ntohs(addr.sin_port));
 
 		conn = smooth_manager_get_connection();
 
@@ -347,19 +476,19 @@ int smooth_manager_create_listener_sockfd(char * host, short port)
 {
 	int ret;
 	int sockfd;
-	struct sockaddr saddr;
-	struct event;
+	struct sockaddr_in saddr;
+	struct epoll_event event;
 
-	saddr.sa_family = AF_INET;
-	saddr.sa_port = htons(port);
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(port);
 
-	ret = inet_pton(AF_INET, host, &saddr.sa_addr);
+	ret = inet_pton(AF_INET, host, &saddr.sin_addr);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
 		return MSG_ERR_FAILED;
 
-	ret = bind(sockfd, &saddr, sizeof(struct sockaddr));
+	ret = bind(sockfd, (struct sockaddr *)&saddr, sizeof(struct sockaddr));
 	if (ret == -1)
 		return MSG_ERR_FAILED;
 
@@ -380,21 +509,28 @@ int smooth_manager_create_listener_sockfd(char * host, short port)
 
 int main()
 {
-	int nfds, fd;
+	int fds, fd;
+	int i;
+	int ret;
 
 	struct epoll_event event_responed[SMOOTH_CONNECTION_MAX];
 
 	epollfd_waiter = epoll_create(SMOOTH_EPOLL_SIZE_DEFAULT);
 	if (epollfd_waiter == -1)
-		return;
+		return -1;
 
-	smooth_manager_create_listener_sockfd(SMOOTH_HOST_DEFAULT, SMOOTH_PORT_DEFAULT);
+	ret = smooth_manager_create_listener_sockfd(SMOOTH_HOST_DEFAULT, SMOOTH_PORT_DEFAULT);
+	if (ret == -1)
+	{
+		fprintf(stderr, "create listen socket failed");
+		return -1;
+	}
 
 	while (1)
 	{
 		fds = epoll_wait(epollfd_waiter, event_responed, SMOOTH_CONNECTION_MAX, -1);
 		if (fds == -1)
-			return;
+			return -1;
 
 		for (i = 0; i < fds; ++i)
 		{
